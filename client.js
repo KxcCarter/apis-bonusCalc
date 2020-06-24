c22096bef32a5edd8bfa0e8eee6310a18c3f5a3f
Scott Bromander <scottbromander@gmail.com>
2020-06-24 14:02:26 -0500

    initial commit

---
+++ client.js
@@ -0,0 +1,104 @@
+const employees = [
+  {
+    name: 'Atticus',
+    employeeNumber: '2405',
+    annualSalary: '47000',
+    reviewRating: 3,
+  },
+  {
+    name: 'Jem',
+    employeeNumber: '62347',
+    annualSalary: '63500',
+    reviewRating: 4,
+  },
+  {
+    name: 'Scout',
+    employeeNumber: '6243',
+    annualSalary: '74750',
+    reviewRating: 5,
+  },
+  {
+    name: 'Robert',
+    employeeNumber: '26835',
+    annualSalary: '66000',
+    reviewRating: 1,
+  },
+  {
+    name: 'Mayella',
+    employeeNumber: '89068',
+    annualSalary: '35000',
+    reviewRating: 1,
+  },
+];
+
+// YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
+
+// Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
+// This problem is massive! Break the problem down. Use the debugger.
+// What is the fewest lines of code I can write and test to get just a little closer?
+
+// This is not a race. Everyone on your team should understand what is happening.
+// Ask questions when you don't.
+
+for (let employee of employees) {
+  console.log(calcBonus(employee));
+}
+
+function calcBonus(employee) {
+  const newEmployeeObject = {};
+  newEmployeeObject.name = employee.name;
+
+  bonusPercent = calcReviewRatingBonus(employee);
+  bonusPercent += calcEmployeeLengthBonus(employee);
+  bonusPercent -= calcBonusSalaryAdjustment(employee);
+  bonusPercent = calcCapForBonus(bonusPercent); //0.14
+
+  newEmployeeObject.bonusPercent = bonusPercent;
+
+  let totalCompensation = parseInt(employee.annualSalary) * (1 + bonusPercent);
+  newEmployeeObject.totalCompensation = totalCompensation.toFixed(2);
+
+  let totalBonus = parseInt(employee.annualSalary) * bonusPercent;
+  newEmployeeObject.totalBonus = totalBonus;
+
+  return newEmployeeObject;
+}
+
+function calcReviewRatingBonus(employee) {
+  let bonusPercent = 0;
+  if (employee.reviewRating === 5) {
+    bonusPercent = 0.1;
+  } else if (employee.reviewRating === 4) {
+    bonusPercent = 0.06;
+  } else if (employee.reviewRating === 3) {
+    bonusPercent = 0.04;
+  } else {
+    bonusPercent = 0;
+  }
+  return bonusPercent;
+}
+
+function calcEmployeeLengthBonus(employee) {
+  if (employee.employeeNumber.length === 4) {
+    return 0.05;
+  } else {
+    return 0;
+  }
+}
+
+function calcBonusSalaryAdjustment(employee) {
+  if (parseInt(employee.annualSalary) > 65000) {
+    return 0.01;
+  } else {
+    return 0;
+  }
+}
+
+function calcCapForBonus(bonusPercent) {
+  if (bonusPercent > 0.13) {
+    bonusPercent = 0.13;
+  } else if (bonusPercent < 0) {
+    bonusPercent = 0;
+  }
+  return bonusPercent;
+}
